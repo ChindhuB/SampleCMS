6c6323bcf1aa06fcdd898e5311f46d3c7440f834
ChindhuB <chindhubabu@gmail.com>
2020-09-08 00:46:42 -0400

    Commit

--- src/main/java/com/maven/cms/ClinicalManagementSystem/PageObjects/LoginPg.java
+++ src/main/java/com/maven/cms/ClinicalManagementSystem/PageObjects/LoginPg.java
@@ -70,6 +70,8 @@ public class LoginPg {
 	}
 
 	public void clickSignout() {
+		wait.until(ExpectedConditions
+				.visibilityOf(logSignout));
 		pgaction.moveToElement(logSignout).build().perform();
 		logSignout.click();
 	}
